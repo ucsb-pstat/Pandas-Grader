b8bc90966afd97e5339ec8330b53e656a9bc52d9
Simon Mo <xmo@berkeley.edu>
2019-02-06 10:52:18 -0800

    Update k8s script and k8s tempalte

--- k8s.py
+++ k8s.py
@@ -1,14 +1,22 @@
 from kubernetes import client, config
 from time import sleep
 import yaml
+import jinja2
 
 config.load_kube_config()
 api_client = client.ApiClient()
 job_api = client.BatchV1Api(api_client)
 api_instance = client.CoreV1Api(api_client)
 
-name = "pi-with-ttl"
+name = "pandas-grader-worker"
 namespace = "data100-staging"
+
+
+def create_grading_job(tmpl_file, kwargs):
+    with open("GradingJobConfig.yml", "r") as f:
+        raw_str = f.read()
+        tmpl = jinja2.Template(raw_str)
+        create_job_from_str(tmpl.render(**kwargs), namespace)
 
 
 def create_job_from_str(template_str, namespace):
@@ -27,34 +35,43 @@ def check_job_status(name, namespace):
             namespace, label_selector=f"controller-uid={c_uid}"
         ).to_dict()
 
-        pod_name = api_response["items"][0]["metadata"]["name"]
+        all_log = ""
+        pod_names = []
+        for pod in api_response["items"]:
+            pod_name = pod["metadata"]["name"]
+            pod_names.append(pod_name)
 
-        api_response = api_instance.read_namespaced_pod_log(
-            pod_name, namespace, follow=True, _preload_content=False
+            api_response = api_instance.read_namespaced_pod_log(
+                pod_name, namespace, follow=True, _preload_content=False
+            )
+            log_lines = list(api_response.stream())
+            log_content = b"".join(log_lines)
+            log_content = log_content.decode()
+            all_log += pod_name + "========\n" + log_content + "\n"
+        return {"done": True, "log": all_log, "pod_names": pod_names}
+
+
+def delete_job(name, namespace, pod_names):
+    for pod_name in pod_names:
+        api_instance.delete_namespaced_pod(
+            pod_name, namespace, body=client.V1DeleteOptions()
         )
-        log_lines = list(api_response.stream())
-        log_content = b"".join(log_lines)
-        log_content = log_content.decode()
-        return {"done": True, "log": log_content, "pod_name": pod_name}
-
-
-def delete_job(name, namespace, pod_name):
-    api_instance.delete_namespaced_pod(
-        pod_name, namespace, body=client.V1DeleteOptions()
-    )
     job_api.delete_namespaced_job(name, namespace, body=client.V1DeleteOptions())
 
 
 if __name__ == "__main__":
-    with open("ExampleJob.yml", "r") as f:
-        create_job_from_str(f.read(), namespace)
-    status, log = False, ""
-    while status == False:
-        print("checking...")
-        resp = check_job_status(name, namespace)
-        status = resp["done"]
-        log = resp["log"]
-        sleep(0.5)
-    print(log)
+    with open("GradingJobConfig.yml", "r") as f:
+        raw_str = f.read()
+        tmpl = jinja2.Template(raw_str)
+        create_job_from_str(tmpl.render(name=name, api_addr="http://8206e840.ngrok.io"), namespace)
 
-    delete_job(name, namespace, resp["pod_name"])
+    # status, log = False, ""
+    # while status == False:
+    #     print("checking...")
+    #     resp = check_job_status(name, namespace)
+    #     status = resp["done"]
+    #     log = resp["log"]
+    #     sleep(0.5)
+    # print(log)
+
+    # # delete_job(name, namespace, resp["pod_names"])
