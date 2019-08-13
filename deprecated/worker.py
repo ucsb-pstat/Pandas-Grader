f975580dda5ad39656050f23b9ff0de0f5e09dcc
simon-mo <xmo@berkeley.edu>
2019-08-13 06:18:33 +0000

    Wrap worker error

--- deprecated/worker.py
+++ deprecated/worker.py
@@ -36,59 +36,67 @@ def main(api_url):
     log_buffer = StringIO()
     conv = Ansi2HTMLConverter()
 
-    with redirect_stdout(log_buffer), redirect_stderr(sys.stdout):
-        fetched = requests.get(f"{api_url}/api/ag/v1/fetch_job").json()
-        if fetched["queue_empty"]:
-            logging.error("Request queue is empty, no work to do, quitting")
-            return 1
-        print(fetched)
-        skeleton_name = fetched["skeleton"]
-        skeleton_zip = requests.get(f"{api_url}/api/ag/v1/skeleton/{skeleton_name}")
+    try:
+        with redirect_stdout(log_buffer), redirect_stderr(log_buffer):
+            fetched = requests.get(f"{api_url}/api/ag/v1/fetch_job").json()
+            if fetched["queue_empty"]:
+                logging.error("Request queue is empty, no work to do, quitting")
+                return 1
+            print(fetched)
+            skeleton_name = fetched["skeleton"]
+            skeleton_zip = requests.get(f"{api_url}/api/ag/v1/skeleton/{skeleton_name}")
 
-        os.makedirs(GRADING_DIR, exist_ok=True)
-        with open(f"{GRADING_DIR}/{skeleton_name}.zip", "wb") as f:
-            f.write(skeleton_zip.content)
+            os.makedirs(GRADING_DIR, exist_ok=True)
+            with open(f"{GRADING_DIR}/{skeleton_name}.zip", "wb") as f:
+                f.write(skeleton_zip.content)
 
-        proc = subprocess.Popen(["unzip", skeleton_name], cwd=GRADING_DIR)
-        proc.wait()
-        assert proc.returncode == 0, "Unzip failed"
+            proc = subprocess.Popen(["unzip", skeleton_name], cwd=GRADING_DIR)
+            proc.wait()
+            assert proc.returncode == 0, "Unzip failed"
 
-        access_token = fetched["access_token"]
-        backup_id = fetched["backup_id"]
-        job_id = fetched["job_id"]
-        backup_assignment_url = (
-            f"https://okpy.org/api/v3/backups/{backup_id}?access_token={access_token}"
-        )
-        backup_json = requests.get(backup_assignment_url).json()
-        file_dict = backup_json["data"]["messages"][0]["contents"]
-        files_to_grade = []
-        for name, content in file_dict.items():
-            if name == "submit":
-                continue
-            files_to_grade.append(name)
-            with open(f"{GRADING_DIR}/{name}", "w") as f:
-                f.write(content)
-        assert len(files_to_grade) == 1, "Only support grading 1 notebook file"
+            access_token = fetched["access_token"]
+            backup_id = fetched["backup_id"]
+            job_id = fetched["job_id"]
+            backup_assignment_url = f"https://okpy.org/api/v3/backups/{backup_id}?access_token={access_token}"
+            backup_json = requests.get(backup_assignment_url).json()
+            try:
+                file_dict = backup_json["data"]["messages"][0]["contents"]
+            except Exception as e:
+                print(e)
+                print(backup_json)
 
-        os.chdir(GRADING_DIR)
-        okpy_result, path_to_score = gofer_wrangle(
-            gofer.ok.grade_notebook(files_to_grade[0])
-        )
-        # print(res)
-        path_to_score["bid"] = backup_id
-        path_to_score["assignment"] = skeleton_name
-        # report_breakdown_url = f"{api_url}/api/ag/v1/report_result"
-        # requests.post(report_breakdown_url, json=path_to_score)
+            files_to_grade = []
+            for name, content in file_dict.items():
+                if name == "submit":
+                    continue
+                files_to_grade.append(name)
+                with open(f"{GRADING_DIR}/{name}", "w") as f:
+                    f.write(content)
+            assert len(files_to_grade) == 1, "Only support grading 1 notebook file"
 
-        score_content = {
-            "bid": backup_id,
-            "score": okpy_result["total"],
-            "kind": "Total",
-            "message": okpy_result["msg"],
-        }
-        score_endpoint = f"https://okpy.org/api/v3/score/?access_token={access_token}"
-        resp = requests.post(score_endpoint, json=score_content)
-        assert resp.status_code == 200, resp.json()
+            os.chdir(GRADING_DIR)
+            okpy_result, path_to_score = gofer_wrangle(
+                gofer.ok.grade_notebook(files_to_grade[0])
+            )
+            # print(res)
+            path_to_score["bid"] = backup_id
+            path_to_score["assignment"] = skeleton_name
+            # report_breakdown_url = f"{api_url}/api/ag/v1/report_result"
+            # requests.post(report_breakdown_url, json=path_to_score)
+
+            score_content = {
+                "bid": backup_id,
+                "score": okpy_result["total"],
+                "kind": "Total",
+                "message": okpy_result["msg"],
+            }
+            score_endpoint = (
+                f"https://okpy.org/api/v3/score/?access_token={access_token}"
+            )
+            resp = requests.post(score_endpoint, json=score_content)
+            assert resp.status_code == 200, resp.json()
+    except Exception as e:
+        print(e)
 
     print(log_buffer.getvalue())
     print("Sending log to api/ag/v1/report_done")
