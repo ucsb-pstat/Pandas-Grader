2c838decdbf7feaa68b2fd42b0d1fa4cd22c003f
William Huang <wwhuang@umich.edu>
2019-09-13 23:54:36 -0700

    small bugfix

--- pandas_grader/app/views.py
+++ pandas_grader/app/views.py
@@ -131,7 +131,7 @@ def report_done(request: HttpRequest, job_id):
 
     result.save()
     print("Report done")
-    print(request.text)
+    print(request.body.decode())
     return HttpResponse(status=200)
 
 
